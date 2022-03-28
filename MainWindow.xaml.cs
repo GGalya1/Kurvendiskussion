df5239c83381fe545f57abffc7f0fb4f1eee55de
student 04 <stk_lum_04@outlook.de>
2022-03-28 14:53:14 +0200

    Projektdateien hinzufügen.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kurvendiskussion
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Knopf_Click(object sender, RoutedEventArgs e)
+        {
+            double anstieg = Convert.ToDouble(EingM.Text);
+            double yAchsenAbschnitt = Convert.ToDouble(EingB.Text);
+            lineareFunktion f = new lineareFunktion(anstieg, yAchsenAbschnitt); //rufen so ein Konstruktor
+
+
+            string AusgabeText = "Die Nullstelle ist x_n=" + f.berechneNullstelle();
+            AusgabeText += "\nDer Schnittpunkt mit der xAchse liegt bei: (" + f.berechneNullstelle() + "/ 0)";
+            Ausgabe.Text = AusgabeText;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double wert = Convert.ToDouble(WertX.Text);
+
+            double anstieg = Convert.ToDouble(EingM.Text);
+            double yAchsenAbschnitt = Convert.ToDouble(EingB.Text);
+            lineareFunktion f = new lineareFunktion(anstieg, yAchsenAbschnitt);
+
+            string AusgabeText = "Das berechnete y-Wert für gegebenen x-Wert ist = " + f.berechneFunktionswert(wert).ToString();
+
+            Ausgabe.Text = AusgabeText;
+        }
+    }
+}
---
+++ lineareFunktion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kurvendiskussion
+{
+    class lineareFunktion
+    {
+        //Eigenschaften (Properties)
+        private double anstieg { get; set; }
+        private double yAchsenAbschnitt { get; set; }
+
+        //Methoden
+
+        //Konstruktor (muss von außen aursufbar sein, darum "public")
+        public lineareFunktion(double anstieg, double yAchsenAbschnitt) //"double anstieg" existiert nur hier, und das hat nicts mit Eigenschaft "anstieg" zu tun
+        {
+            this.anstieg = anstieg; //"this.anstieg" nihm die Eigenschaft mit diesem Namen und das soll mit Variable die nach "=" eingesetzt werden
+            this.yAchsenAbschnitt = yAchsenAbschnitt;
+        }
+
+        //weitere Methoden
+        public double berechneNullstelle()
+        {
+            double nullstelle;
+            nullstelle = -yAchsenAbschnitt / anstieg;
+            return nullstelle;
+        }
+
+        public double berechneFunktionswert(double XWert)
+        {
+            double Funktionswert;
+            Funktionswert=anstieg* XWert + yAchsenAbschnitt;
+            return Funktionswert;
+        }
+    }
+}
